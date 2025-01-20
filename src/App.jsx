60a04231d9524d2e05ea3ecace97315ffb72a005
luigi-giordano <luigi.giordano1992@gmail.com>
2025-01-20 15:15:05 +0100

    creazione array di oggetti e opzioni disponibili

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,10 @@ import TodoListForm from './components/TodoListForm';
 
 const FormBase = () => {
 
-  // const [firstName, setFirstName] = useState('');
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(firstName)
-  // }
-
   return (
     <div className="container my-5">
 
       <TodoListForm />
-
-      {/* <div className="title my-5"><h2>Inserimento Nomi: {firstName}</h2></div>
-      <div className="row">
-        <div className="col-6">
-          <form
-            action="#"
-            onSubmit={handleSubmit}
-          >
-            <input
-              className="form-control"
-              type="text"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              placeholder="Inserisci Nome"
-            />
-            <button
-              className="btn btn-primary my-5 w-100"
-            >Invio</button>
-          </form>
-        </div>
-      </div> */}
 
     </div>
   );
--- src/components/TodoListForm.jsx
+++ src/components/TodoListForm.jsx
@@ -1,69 +1,45 @@
 import { useState } from 'react';
 
 const initialList = [
-  'Acquisti in fumetteria',
-  'Fare la spesa',
-  'Finire il progetto di lavoro',
-  'Portare fuori la spazzatura'
+  {
+    title: 'Acquisti in fumetteria',
+    content: 'Comprare manga e fumetti preferiti.',
+    category: 'Hobby',
+    tags: ['Tempo libero', 'Shopping'],
+    image: '',
+    published: true,
+  },
+  {
+    title: 'Fare la spesa',
+    content: 'Lista di alimenti e prodotti da comprare.',
+    category: 'Casa',
+    tags: ['Cibo', 'Casa'],
+    image: '',
+    published: false,
+  },
 ];
 
+// Opzioni disponibili per categoria e tag
+const categories = ['Hobby', 'Casa', 'Lavoro', 'Altro'];
+const tags = ['Tempo libero', 'Shopping', 'Cibo', 'Casa', 'Ufficio'];
+
 const TodoListForm = () => {
-
+  // Stato della lista delle attività
   const [todoList, setTodoList] = useState(initialList);
-  const [newTodoList, setNewTodoList] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Creazione clone todolist e aggiungo il nuovo elemento
-    const newList = [...todoList, newTodoList];
-    // Aggiorno la todolist
-    setTodoList(newList);
-  }
 
-  const handleRemoveList = (index) => {
-    // Creazione clone todolist
-    const newListList = todoList.filter((item, i) => i !== index);
-    // Aggiorno la todolist
-    setTodoList(newListList);
-  }
 
-  return (
-    <div className="container my-5">
 
-      <form action="#" onSubmit={handleSubmit}>
-        <div className='title d-flex mb-3 justify-content-center'><h2>Il tuo Blog</h2></div>
 
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            value={newTodoList}
-            onChange={(e) => setNewTodoList(e.target.value)}
-            placeholder="Inserisci un nuovo elemento nella lista"
-          />
-          <button
-            className="btn btn-outline-secondary btn-primary text-white"
-            type="submit"
-          >Aggiungi</button>
-        </div>
 
-      </form>
 
-      <>
-        <ul className="list-group">
-          {todoList.map((item, index) => (
-            <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-              <span>{item}</span>
-              <i
-                className="fa-solid fa-trash pointer"
-                onClick={() => handleRemoveList(index)}>
-              </i>
-            </li>
-          ))}
-        </ul>
-      </>
 
-    </div>
-  )
+
+
+
+
+
+
+
 }
 export default TodoListForm;
